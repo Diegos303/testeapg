d18c619d9de3a3ffca3b3c1f8170ba50bf45a6f5
diego <diego.melo4@fatec.sp.gov.br>
2025-09-24 23:41:05 -0300

    primeira versão

---
+++ JogoV1.java
@@ -0,0 +1,10 @@
+public class JogoV1 {
+    public static void main (String[] args) {
+        Personagem kawaii = new Personagem();
+        kawaii.nome = "nico";
+        kawaii.cacar();
+        kawaii.comer();
+        kawaii.dormir();
+    }
+    
+}
---
+++ Personagem.java
@@ -0,0 +1,20 @@
+public class Personagem {
+    String nome;
+    int energia;
+    int fome;
+    int sono;
+
+    void cacar() {
+        System.out.println(nome +" caçando.......");
+    }
+    void dormir() {
+        System.out.println(nome + "dormindo....");
+    }
+    void comer() {
+        System.out.println(nome + "comendo.....");
+
+    }
+
+
+    
+}
